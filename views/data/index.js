bd6061fc25c2288b6ae7e7919cd4fc5257c404a8
mathewhoyerstokes <57637121+mathewhoyerstokes@users.noreply.github.com>
2022-05-04 13:48:50 +0200

    [CHANGE] Member data table templating with static content

--- views/data/index.js
+++ views/data/index.js
@@ -1,38 +1,106 @@
-import { Heading, Box, Grid, Text } from "@unioncredit/ui";
+import { Heading, Box, Text } from "@unioncredit/ui";
 import useIsMobile from "hooks/useIsMobile";
 import { Footer } from "components-ui";
+import style from "./dataTable.module.css";
 
 export default function StatsView() {
   const isMobile = useIsMobile();
 
   return (
-    <Grid>
-      <Grid.Row>
-        <Grid.Col>
-          <Box
-            mb="56px"
-            fluid
-            align="center"
-            justify="center"
-            direction="vertical"
-          >
-            <Heading
-              size={isMobile ? "large" : "xxlarge"}
-              align="center"
-              mt={"60px"}
-              className={"text--grey700"}
-            >
-              Union Network Statistics
-            </Heading>
+    <div>
+      <Box mb="56px" fluid align="center" justify="center" direction="vertical">
+        <Heading
+          size={isMobile ? "large" : "xxlarge"}
+          align="center"
+          mt={"60px"}
+          className={"text--grey700"}
+        >
+          Union Network Statistics
+        </Heading>
 
-            <Text align="center" mb="24px" className={"text--grey400"}>
-              Multi-chain statistics and analysis related to Union Protocol
-            </Text>
-          </Box>
-        </Grid.Col>
-      </Grid.Row>
+        <Text align="center" mb="24px" className={"text--grey400"}>
+          Multi-chain statistics and analysis related to Union Protocol
+        </Text>
+      </Box>
+
+      <table className={style.tableWrapper}>
+        <tbody className={style.border}>
+          <tr className={style.tableHeaderRow}>
+            <td className={style.headerItemEmpty}></td>
+            <td className={style.headerItemAccount}>Account</td>
+            <td className={style.headerItem}>Vouches</td>
+            <td className={style.headerItem}>Trust (DAI)</td>
+            <td className={style.headerItem}>Available (DAI)</td>
+            <td className={style.headerItem}>Balance Owed (DAI)</td>
+            <td className={style.headerItem}>Loan Status</td>
+            <td className={style.headerItem}>Staked (DAI)</td>
+            <td className={style.headerItem}>Backing</td>
+            <td className={style.headerItem}>Utilized Stake (DAI)</td>
+            <td className={style.headerItem}>Frozen Stake (DAI)</td>
+          </tr>
+          <tr className={style.tableBodyRow}>
+            <td className={style.ensImage}>
+              <img src="/images/ens-placeholder.png" />
+              <img className={style.isMember} src="/images/member-check.png" />
+            </td>
+            <td className={style.bodyItemAccount}>
+              <span className={style.ensName}>0xtestssss.eth</span>
+              <span className={style.address}>0xxdvWJ...g68QG</span>
+            </td>
+            <td className={style.bodyItem}>10</td>
+            <td className={style.bodyItem}>2935.09</td>
+            <td className={style.bodyItem}>1177.74</td>
+            <td className={style.bodyItem}>402.67</td>
+            <td className={style.bodyItem}>badge</td>
+            <td className={style.bodyItem}>8480.06</td>
+            <td className={style.bodyItem}>24</td>
+            <td className={style.bodyItem}>167.89</td>
+            <td className={style.bodyItem}>1</td>
+          </tr>
+
+          <tr className={style.tableBodyRow}>
+            <td className={style.ensImage}>
+              <img src="/images/ens-placeholder.png" />
+              <img className={style.isMember} src="/images/member-check.png" />
+            </td>
+            <td className={style.bodyItemAccount}>
+              <span className={style.ensName}>elses.eth</span>
+              <span className={style.address}>0xxdvWJ...g68QG</span>
+            </td>
+            <td className={style.bodyItem}>10</td>
+            <td className={style.bodyItem}>2935.09</td>
+            <td className={style.bodyItem}>1177.74</td>
+            <td className={style.bodyItem}>402.67</td>
+            <td className={style.bodyItem}>badge</td>
+            <td className={style.bodyItem}>8480.06</td>
+            <td className={style.bodyItem}>24</td>
+            <td className={style.bodyItem}>167.89</td>
+            <td className={style.bodyItem}>1</td>
+          </tr>
+
+          <tr className={style.tableBodyRow}>
+            <td className={style.ensImage}>
+              <img src="/images/ens-placeholder.png" />
+              <img className={style.isMember} src="/images/member-check.png" />
+            </td>
+            <td className={style.bodyItemAccount}>
+              <span className={style.ensName}>0xmunter.eth</span>
+              <span className={style.address}>0xxdvWJ...g68QG</span>
+            </td>
+            <td className={style.bodyItem}>10</td>
+            <td className={style.bodyItem}>2935.09</td>
+            <td className={style.bodyItem}>1177.74</td>
+            <td className={style.bodyItem}>402.67</td>
+            <td className={style.bodyItem}>badge</td>
+            <td className={style.bodyItem}>8480.06</td>
+            <td className={style.bodyItem}>24</td>
+            <td className={style.bodyItem}>167.89</td>
+            <td className={style.bodyItem}>1</td>
+          </tr>
+        </tbody>
+      </table>
 
       <Footer></Footer>
-    </Grid>
+    </div>
   );
 }
