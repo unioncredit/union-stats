b8ce4290913684a42d780d076f1ecfca16bba0f1
mathewhoyerstokes <57637121+mathewhoyerstokes@users.noreply.github.com>
2022-05-04 16:29:28 +0200

    [CHANGE] remove page view

--- pages/network.js
+++ pages/network.js
@@ -1,11 +1,9 @@
-import DataView from "views/data";
 import { PageHead } from "components-ui";
 
 export default function StatsPage() {
   return (
     <>
       <PageHead title="Union Stats" />
-      <DataView></DataView>
     </>
   );
 }
