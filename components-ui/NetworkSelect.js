77abb71fdf953839c5ddaffb10a5ffdefac3a95f
mathewhoyerstokes <57637121+mathewhoyerstokes@users.noreply.github.com>
2022-05-12 09:24:24 +0200

    [FIX] default value index given to network select to keep state on page switch

--- components-ui/NetworkSelect.js
+++ components-ui/NetworkSelect.js
@@ -5,6 +5,7 @@ import { chainIdState } from "hooks/useChainId";
 
 export function NetworkSelect() {
   const [loading] = useState(false);
+  const chainId = chainIdState.useValue([]);
 
   const handleChangeNetwork = (value) => {
     if (value.chainId) {
@@ -12,13 +13,17 @@ export function NetworkSelect() {
     }
   };
 
+  const defaultValueIndex = options.findIndex(
+    (option) => option.chainId === chainId
+  );
+
   return (
     <Select
       options={options}
       mb="20px"
       onChange={handleChangeNetwork}
       isLoading={loading}
-      defaultValue={options[0]}
+      defaultValue={!!~defaultValueIndex && options[defaultValueIndex]}
     />
   );
 }
