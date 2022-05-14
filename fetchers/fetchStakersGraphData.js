4e7bf075610fb928b7fd524f4339e148026c3021
Gerald <gm@gerald.host>
2022-05-14 17:34:15 +0100

    feat: unioncredit/data

--- fetchers/fetchStakersGraphData.js
+++ fetchers/fetchStakersGraphData.js
@@ -1,59 +1,23 @@
 import { ethers } from "ethers";
 import groupBy from "lodash/groupBy";
-import sumBy from "lodash/sumBy";
-import getLogs from "./fetchLogs";
-import fetchUnstakersGraphData from "./fetchUnstakersGraphData";
-
-const topicHash =
-  "0x3dbdcfd4c1f2e08931aae3d544e149a1e643143f5234d166fe3debb783388495";
+import { fetchUserManagerMeta } from "@unioncredit/data";
 
 export default async function fetchStakersGraphData() {
-  const json = await getLogs(topicHash);
+  const result = await fetchUserManagerMeta();
 
-  const data = json.data.items;
-
-  const parsed = data.map((item) => {
-    const staker = ethers.BigNumber.from(item.raw_log_topics[1]).toHexString();
-    const amount = ethers.BigNumber.from(item.raw_log_data);
-    const date = new Date(item.block_signed_at);
-    const day = new Date(date.getFullYear(), date.getMonth(), date.getDate());
-
-    return { amount, staker, day };
+  const grouped = groupBy(result, (row) => {
+    const date = new Date(Number(row.timestamp) * 1000);
+    return new Date(date.getFullYear(), date.getMonth(), date.getDate());
   });
 
-  const grouped = groupBy(parsed, (x) => x.day.toString());
+  const aggregate = Object.keys(grouped).map((key) => {
+    const row = grouped[key];
+    const last = row[row.length - 1];
+    return { totalStaked: last.totalStaked, timestamp: last.timestamp };
+  });
 
-  let prevSum = 0;
-  const agg = Object.keys(grouped).reduce((acc, key) => {
-    const group = grouped[key];
-    // sum the group
-    const sumOfGroup = sumBy(group, (x) =>
-      Number(ethers.utils.formatEther(x.amount))
-    );
-
-    const amount = sumOfGroup;
-    const n = { ...acc, [key]: amount + prevSum };
-    prevSum += amount;
-    return n;
-  }, {});
-
-  let dateRange = [];
-  const aggKeys = Object.keys(agg);
-  const startDate = new Date(aggKeys[0]);
-  const endDate = Date.now();
-  for (let d = startDate; d <= endDate; d.setDate(d.getDate() + 1)) {
-    dateRange.push(new Date(d));
-  }
-
-  const unstakeGraphData = await fetchUnstakersGraphData();
-
-  let graphData = {};
-  let lastSum = 0;
-  for (let i = 0; i < dateRange.length; i++) {
-    const date = dateRange[i];
-    lastSum = agg[date] || lastSum;
-    graphData[date] = { y: lastSum - unstakeGraphData[date]?.y || 0, x: date };
-  }
-
-  return Object.values(graphData);
+  return aggregate.map((row) => ({
+    x: new Date(Number(row.timestamp) * 1000),
+    y: Number(ethers.utils.formatEther(row.totalStaked)),
+  }));
 }
