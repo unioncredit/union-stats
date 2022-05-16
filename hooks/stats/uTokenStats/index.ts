c453c0f3ab8fb8bcc17764606e745c6c1fde855b
Gerald <gm@gerald.host>
2022-05-16 01:31:05 +0100

    fix: remove bad import

--- hooks/stats/uTokenStats/index.ts
+++ hooks/stats/uTokenStats/index.ts
@@ -3,7 +3,6 @@ import useTotalRedeemable from "./useTotalRedeemable";
 import useTotalReserves from "./useTotalReserves";
 import useUTokenSupply from "./useUTokenSupply";
 import useUTokenRate from "./useUTokenRate";
-import { useDefaultedAmount } from "./useDefaultedAmount";
 
 export default function useUTokenStats() {
   const { data: totalBorrows } = useTotalBorrows();
