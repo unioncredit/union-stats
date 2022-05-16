f7344785a5f0049edf3fababdac65ea18b24d6b8
Gerald <gm@gerald.host>
2022-05-16 15:26:41 +0100

    fix: invariant error

--- src/hooks/useContract.js
+++ src/hooks/useContract.js
@@ -1,25 +1,11 @@
 import { isAddress } from "@ethersproject/address";
-import { BigNumber } from "@ethersproject/bignumber";
 import { Contract } from "@ethersproject/contracts";
-import { useWeb3React } from "@web3-react/core";
 import { useMemo } from "react";
 
 export default function useContract(address, ABI, provider) {
-  const { library, account } = useWeb3React();
-
   return useMemo(() => {
-    if (BigNumber.from(address).eq("0")) {
-      return undefined;
-    }
-
-    const lib = provider
-      ? provider
-      : account
-      ? library?.getSigner(account)
-      : library;
-
-    return isAddress(address) && !!ABI && !!lib
-      ? new Contract(address, ABI, lib)
+    return isAddress(address) && !!ABI && !!provider
+      ? new Contract(address, ABI, provider)
       : undefined;
-  }, [address, ABI, library, account, provider]);
+  }, [address, ABI, provider]);
 }
