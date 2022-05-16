92242e8fb04968b08e9e227d4e914a99c9973151
mathewhoyerstokes <57637121+mathewhoyerstokes@users.noreply.github.com>
2022-05-16 12:09:26 +0200

    [CHANGE] Looking for Bug in regards to a deploy

--- views/stats/index.js
+++ views/stats/index.js
@@ -1,23 +1,9 @@
 import { Grid, Header } from "@unioncredit/ui";
-import UnionTokenStats from "./UnionTokenStats";
-import UTokenStats from "./UTokenStats";
-import UserManagerStats from "./UserManagerStats";
-import AssetManagerStats from "./AssetManagerStats";
-import MarketSettingsStats from "./MarketSettingsStats";
-import GovernanceStats from "./GovernanceStats";
-import TreasuryStats from "./TreasuryStats";
+
 import { Navigation, NetworkSelect } from "components";
 import style from "./stats.module.css";
-import useChainId from "../../hooks/useChainId";
 
 export default function StatsView() {
-  const chainId = useChainId();
-  let governenceStatCard;
-
-  if (chainId === 1) {
-    governenceStatCard = <GovernanceStats></GovernanceStats>;
-  }
-
   return (
     <div className={style.protocolPageWidth}>
       <Header className={style.memberHeader}>
@@ -28,19 +14,7 @@ export default function StatsView() {
         <NetworkSelect></NetworkSelect>
       </div>
 
-      <Grid.Row>
-        <Grid.Col>
-          <UserManagerStats></UserManagerStats>
-          <UTokenStats></UTokenStats>
-          <div>{governenceStatCard}</div>
-          <UnionTokenStats></UnionTokenStats>
-        </Grid.Col>
-        <Grid.Col>
-          <AssetManagerStats></AssetManagerStats>
-          <MarketSettingsStats></MarketSettingsStats>
-          <TreasuryStats></TreasuryStats>
-        </Grid.Col>
-      </Grid.Row>
+      <Grid.Row></Grid.Row>
     </div>
   );
 }
