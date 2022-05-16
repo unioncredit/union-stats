f5ecb07ac87a93007da469475c9b066cfaea3166
mathewhoyerstokes <57637121+mathewhoyerstokes@users.noreply.github.com>
2022-05-16 12:16:55 +0200

    [CHANGE] Looking for Bug in regards to a deploy

--- views/stats/index.js
+++ views/stats/index.js
@@ -1,10 +1,8 @@
 import { Grid, Header } from "@unioncredit/ui";
-import UnionTokenStats from "./UnionTokenStats";
-import UTokenStats from "./UTokenStats";
 
 import MarketSettingsStats from "./MarketSettingsStats";
 import GovernanceStats from "./GovernanceStats";
-import TreasuryStats from "./TreasuryStats";
+
 import { Navigation, NetworkSelect } from "components";
 import style from "./stats.module.css";
 import useChainId from "../../hooks/useChainId";
@@ -29,13 +27,10 @@ export default function StatsView() {
 
       <Grid.Row>
         <Grid.Col>
-          <UTokenStats></UTokenStats>
           <div>{governenceStatCard}</div>
-          <UnionTokenStats></UnionTokenStats>
         </Grid.Col>
         <Grid.Col>
           <MarketSettingsStats></MarketSettingsStats>
-          <TreasuryStats></TreasuryStats>
         </Grid.Col>
       </Grid.Row>
     </div>
