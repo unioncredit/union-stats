fc09a750b67f2df182019aabf1397ed282b7ddc0
twygod <twygod@gmail.com>
2022-08-03 15:16:28 +0800

    chg:  Display one decimal place while it loads.

--- src/views/stats/values.js
+++ src/views/stats/values.js
@@ -1,12 +1,17 @@
 import { Dai, Union, DaiNoComma } from "components";
 import format from "util/formatValue";
 
-export const commifyNoDecimals = (value, digits = 4) => (
-  <DaiNoComma value={format(value || 0, digits)} />
+export const commifyNoDecimals = (value, digits) => (
+  <DaiNoComma value={format(value || 0, digits ? digits : value ? 4 : 1)} />
 );
 
-export const unionValue = (value, digits = 4, symbol = "UNION") => (
-  <Union value={format(value || 0, digits)} symbol={symbol} />
+export const unionValue = (value, digits, symbol = "UNION") => (
+  <Union
+    value={format(value || 0, digits ? digits : value ? 4 : 1)}
+    symbol={symbol}
+  />
 );
 
-export const daiValue = (value, digits = 4) => <Dai value={format(value || 0, digits)} />;
+export const daiValue = (value, digits) => (
+  <Dai value={format(value || 0, digits ? digits : value ? 4 : 1)} />
+);
