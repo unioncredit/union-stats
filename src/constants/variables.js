de28015b9ae4ad1cc99b054d957b555df2fd9eea
Jacob Shiach <kingjacob@gmail.com>
2023-04-24 19:38:14 -0500

    update variables to new opgoe deployment

--- src/constants/variables.js
+++ src/constants/variables.js
@@ -11,25 +11,25 @@ export const BLOCKS_PER_YEAR = {
 
 export const ASSET_MANAGER_ADDRESSES = {
   [chain.mainnet.id]: "0xb91a874D9AA8fF7E478bA61286ECc77c1A3E384d",
-  [chain.opgoerli.id]: "0x099a2B3f5A62053D6Aa032A9AeC410f7d51713e3",
+  [chain.opgoerli.id]: "0x8E92930536764DF03ee26079e8D8A57a9f3a833b",
   [chain.arbitrum.id]: "0x7Aecd107Cb022e1DFd42cC43E9BA94C38BC83275",
 };
 
 export const MARKET_REGISTRY_ADDRESSES = {
   [chain.mainnet.id]: "0x1ddB9a1F6Bc0dE1d05eBB0FDA61A7398641ae6BE",
-  [chain.opgoerli.id]: "0x7A275FD7676B6Cd6C04e45718D7cc530Ea485300",
+  [chain.opgoerli.id]: "0x00fDD2c77b5Cb6aCCd26eF299692f952f555DDb3",
   [chain.arbitrum.id]: "0x82c7cA392644a6c66fcaF9d4efF89e6d875D58D9",
 };
 
 export const USER_MANAGER_ADDRESSES = {
   [chain.mainnet.id]: "0x49c910Ba694789B58F53BFF80633f90B8631c195",
-  [chain.opgoerli.id]: "0x55a095033FFECcF4E8f712ebda0850c7f3dF2E25",
+  [chain.opgoerli.id]: "0xe2732f6E7306908697D111A53806C5883eaf0fc5",
   [chain.arbitrum.id]: "0xb71F3D4342AaE0b8D531E14D2CF2F45d6e458A5F",
 };
 
 export const UNION_TOKEN_ADDRESSES = {
   [chain.mainnet.id]: "0x5Dfe42eEA70a3e6f93EE54eD9C321aF07A85535C",
-  [chain.opgoerli.id]: "0x457E066FC7D6c7567f21Fc96E7a713d02cE58D61",
+  [chain.opgoerli.id]: "0xa5DaCCAf7E72Be629fc0F52cD55d500Fd6fa7677",
   [chain.arbitrum.id]: "0x6DBDe0E7e563E34A53B1130D6B779ec8eD34B4B9",
 };
 
@@ -41,13 +41,13 @@ export const UNION_WRAPPED_TOKEN_ADDRESSES = {
 
 export const COMPTROLLER_ADDRESSES = {
   [chain.mainnet.id]: "0x216dE4089dCdD7B95BC34BdCe809669C788a9A5d",
-  [chain.opgoerli.id]: "0x1c970FAC1D3BDcB9Bc984A2f39Df769760AeE0ac",
+  [chain.opgoerli.id]: "0x4A89d70e17F9e765077dfF246c84B47c1181c473",
   [chain.arbitrum.id]: "0x641DD6258cb3E948121B10ee51594Dc2A8549fe1",
 };
 
 export const U_TOKEN_ADDRESSES = {
   [chain.mainnet.id]: "0x954F20DF58347b71bbC10c94827bE9EbC8706887",
-  [chain.opgoerli.id]: "0x10f35722588571BB109820Be6F515634336512Ec",
+  [chain.opgoerli.id]: "0xDe16E91e6EF17D29F4f5e6DAA5E7827A3CaA6F29",
   [chain.arbitrum.id]: "0x954F20DF58347b71bbC10c94827bE9EbC8706887",
 };
 
@@ -89,7 +89,7 @@ export const COMPOUND_ADAPTER_ADDRESSES = {
 
 export const PURE_TOKEN_ADAPTER_ADDRESSES = {
   [chain.mainnet.id]: "0x62DD06026F5f8e874eEfF362b1280CD9A2057b7d",
-  [chain.opgoerli.id]: "0x679adbbe5cb73ddee93dec5d2d52f16950344da4",
+  [chain.opgoerli.id]: "0x80Fd562D33B0f63bea21541007cD4D26D3832e56",
   [chain.arbitrum.id]: "0xdC3c984f2Ecb7Ee2540bb0B9EfE9540204cdAB57",
 };
 
@@ -101,7 +101,7 @@ export const AAVE_ADAPTER_ADDRESSES = {
 
 export const AAVE_V3_ADAPTER_ADDRESSES = {
   [chain.mainnet.id]: AddressZero,
-  [chain.opgoerli.id]: "0x2B2f7A61489A0Aa8638790125DE0DDAD9CB5D12e",
+  [chain.opgoerli.id]: "0xE5C43c996231a35d5c81D5F9Ccb03769d054d40c",
   [chain.arbitrum.id]: "0x393d7299c2caa940b777b014a094c3b2ea45ee2b",
 };
 
@@ -119,7 +119,7 @@ export const ARB_UNION_WRAPPER = {
 
 export const BLOCK_SPEED = {
   [chain.mainnet.id]: 12,
-  [chain.opgoerli.id]: 2,
+  [chain.opgoerli.id]: 1,
   [chain.arbitrum.id]: 13,
 };
 
